4ce2a2ca30eaea92972bedbd574b84cc2bf97233
Joaof14 <joao-pedro032011@hotmail.com>
2023-09-25 17:47:36 -0300

    'começando a construir rede neural'

--- MLearning1/Newtork.py
+++ MLearning1/Newtork.py
@@ -1,7 +1,37 @@
 from keras.datasets import mnist
+import numpy as np
+import random
 
+(train_X, train_y), (test_X, text_Y) = mnist.load_data()
 
-class Network:
+class CrossEntropyCost:
     def __init__(self) -> None:
         pass
 
+class Network:
+    def __init__(self, sizes, cost = CrossEntropyCost):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.cost = cost
+        self.default_weight_initializer() 
+
+    def default_weight_initializer(self):
+        self.weights = [np.random.rand(y,1) for y in self.sizes[1:]]
+        self.biases = [np.random.rand(y,x)/np.sqrt(x) for x,y in zip(self.sizes[:-1], self.sizes[:1])]
+
+    def feedforward(self,z):
+        for w,b in self.weights, self.biases:
+            a = ativFunc(np.dot(a,w) + b)
+        return a
+
+
+
+
+
+def ativFunc(z):
+    return 1/(1-np.exp(-z))
+    #função sigmoide
+
+def dativFunc(z):
+    ativFunc(z)*(1-ativFunc(z))
+    #função sigmoide
